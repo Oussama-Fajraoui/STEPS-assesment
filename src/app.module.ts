3959601252a9805634fac944df1e4f176d69c4c7
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2024-04-16 20:31:51 +0100

    create blog module controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI)],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
