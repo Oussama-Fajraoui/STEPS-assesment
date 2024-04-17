81831547df6195e78cfbbaf01ab1d819c98a2b26
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2024-04-17 09:24:26 +0100

    Auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
+import { AuthModule } from './auth/auth.module';
 
 console.log('MongoDB URI:', process.env.MONGODB_URI);
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb+srv://oussamagf:24082007g@cluster0.is9uh6v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"), BlogModule],
+  imports: [MongooseModule.forRoot("mongodb+srv://oussamagf:24082007g@cluster0.is9uh6v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"), BlogModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
